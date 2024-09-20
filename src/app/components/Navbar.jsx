613bd8512f503c83e7f9c01ee1b6b030cc80ba07
Your Name <you@example.com>
2024-09-20 18:13:36 +0100

    navigation bar width reduced for mobile and tablet

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
             animate={{ opacity: 1, height: 'auto' }}
             exit={{ opacity: 0, height: 0 }}
             transition={{ duration: 0.3 }}
-            className="md:hidden bg-black"
+            className="md:hidden bg-black w-[70%] right-0 absolute"
           >
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               {navLinks.map((link, index) => (
